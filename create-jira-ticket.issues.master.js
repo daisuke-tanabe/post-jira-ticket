37628a8f33849c62f4a065c4fc00314c8fbab634
daisuke-tanabe <helloworld0840@gmail.com>
2021-08-30 19:37:15 +0900

    feat: describe dummy data in issue file

--- create-jira-ticket.issues.master.js
+++ create-jira-ticket.issues.master.js
@@ -1,1 +1,28 @@
-export const issues = []
+export const issues = [
+  {
+    fields: {
+      // プロジェクトid
+      project: { id: '' },
+      // 説明
+      description: '',
+      // 要約
+      summary: '',
+      // 課題タイプ
+      issuetype: {
+        id: '',
+      },
+      // ラベル
+      labels: [''],
+      // 修正バージョン
+      fixVersions: [{ id: '' }],
+      // エピックリンク
+      customfield_10005: '',
+      // 担当者
+      assignee: {
+        name: '',
+      },
+      // 期限
+      duedate: '',
+    }
+  },
+]
